ff537b93bcc7d53ce8801a16f139d2c7627ecd7c
Rachelsep <rachelann.sepulveda@gmail.com>
2021-07-21 17:24:04 -0700

    updated icon css

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="black"
+      color="#212529"
       size="64px"
       animate={true}
     />
